73fd1b7f2421039ab3a8a445ed5a33daa08de775
trubb <trubb@example.com>
2022-04-17 19:58:09 +0200

    working solution

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # trubb_markerparser
-Parses marker arrays generated from Sweet markers system, bookmark arrays from Tun-Firesupport, and translates between the two
+
+Parses marker arrays generated from [Sweet markers system](https://steamcommunity.com/sharedfiles/filedetails/?id=324952672) and generates a trp array for use with [Tuntematon Firesupport](https://github.com/tuntematonjr/Tun-Firesupport).
+
+Executable files will be found under releases.
+
+To build from source, simply clone the repo, run `go get`, followed by `go build`.
+
+```yaml
+NAME:
+   markerparser - Translate between marker and artillery target/bookmark arrays
+
+USAGE:
+   markerparser [global options] command [command options] [arguments...]
+
+COMMANDS:
+   toTuntematonFireSupport, totfs  from sweet marker array to tuntematon firesupport trps
+   OPTIONS:
+      --sourceFile FILE, -s FILE  load array from FILE
+
+GLOBAL OPTIONS:
+   --help, -h  show help (default: false)
+```
--- main.go
+++ main.go
@@ -1,42 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
-	"sort"
+	"strings"
+	"time"
 
 	"github.com/urfave/cli/v2"
 )
 
 var fileName string
 
+type tun struct {
+	name   string
+	xCoord string
+	yCoord string
+}
+
 func main() {
-
 	app := &cli.App{
-
-		Name:  "Markerparser",
-		Usage: "Translate between marker and artillery target/bookmark arrays",
+		Name:  "markerparser",
+		Usage: "Translate between marker and artillery trp arrays",
 		Commands: []*cli.Command{
 			{
-				Name:    "tuntematonFireSupport",
-				Aliases: []string{"tfs"},
+				Name:    "toTuntematonFireSupport",
+				Aliases: []string{"totfs"},
 				Usage:   "from sweet marker array to tuntematon firesupport trps",
 				Action:  sweetToTun,
-				Flags: []cli.Flag{
-					&cli.StringFlag{
-						Name:        "sourceFile",
-						Aliases:     []string{"s"},
-						Usage:       "load array from `FILE`",
-						Destination: &fileName,
-					},
-				},
-			},
-			{
-				Name:    "sweetMarkerSystem",
-				Aliases: []string{"sms"},
-				Usage:   "from tuntematon firesupport trps to sweet marker array",
-				Action:  tunToSweet,
 				Flags: []cli.Flag{
 					&cli.StringFlag{
 						Name:        "sourceFile",
@@ -49,53 +41,137 @@ func main() {
 		},
 	}
 
-	sort.Sort(cli.FlagsByName(app.Flags))
-	sort.Sort(cli.CommandsByName(app.Commands))
-
 	err := app.Run(os.Args)
 	if err != nil {
-		log.Fatalf(err)
+		log.Fatal(err)
 	}
 }
 
-func tunToSweet(c *cli.Context) error {
-	log.Printf("Trying to run tunToSweet with the filename %q", fileName)
-	return nil
-}
-
+// sweetToTun transforms a provided Sweet Markers array into Tuntematon Firesupport's target reference point format.
 func sweetToTun(c *cli.Context) error {
-	log.Printf("Trying to run sweetToTun with the filename %q", fileName)
-	return nil
-}
 
-func writeToFile(fileName string, input string) error {
+	log.Printf("Running sweetToTun on file %q", fileName)
 
-	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	var trps []tun
+	var trpString strings.Builder
+	trpString.WriteString("[[],[")
+
+	content, err := readFromFile(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// clean
+	content = strings.ReplaceAll(content, "\n", "")
+	content = strings.ReplaceAll(content, "\t", "")
+	content = strings.ReplaceAll(content, " ", "")
+
+	// break up into individual entries
+	entries := strings.Split(content, "],[")
+
+	// extract the three first parts of each entry
+	for _, str := range entries {
+		entry := strings.ReplaceAll(str, "[", "")
+		entry = strings.ReplaceAll(entry, "]", "")
+
+		components := strings.Split(entry, ",")
+
+		// ignore any "" entry, as those are impossible to reference accurately
+		if components[0] == "\"\"" {
+			continue
+		}
+
+		// wrangle the coordinates so that there's zeroes in front!
+		for j := 1; j <= 2; j++ {
+			if strings.Contains(components[j], ".") {
+
+				// discard decimals
+				k := strings.Index(components[j], ".")
+
+				// add enough zeroes to the front of the string to pad up to 5 characters
+				var sb strings.Builder
+				pad := strings.Repeat("0", 5-len(components[j][:k]))
+				sb.WriteString("\"" + pad + components[j][:k] + "\"")
+
+				components[j] = sb.String()
+			}
+		}
+
+		trps = append(trps, tun{components[0], components[1], components[2]})
+	}
+
+	for _, trp := range trps {
+		trpString.WriteString("[" + trp.name + "," + trp.xCoord + "," + trp.yCoord + "],")
+	}
+
+	final := strings.TrimRight(trpString.String(), ",")
+	final += "]]"
+
+	log.Println("Parsed the input to the following trp array:")
+	fmt.Println("\n" + final + "\n")
+
+	outFile := createFile()
+	err = writeToFile(outFile, final)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Finished")
+
+	return nil
+}
+
+// createFile creates a text file with a supposedly unique filename.
+// If another file with the same name already exists, that file will be deleted.
+func createFile() string {
+
+	fileName := "parsed_markers_" + time.Now().Format("2006-01-02T15:04:05Z07:00") + ".txt" // RFC3339 format
+
+	err := os.Remove(fileName)
+	if err != nil {
+		log.Println("No file with same name found, creating...")
+	} else {
+		log.Println("Deleted pre-existing file with same name")
+	}
+
+	_, err = os.Create(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Created output file %v\n", fileName)
+
+	return fileName
+}
+
+// writeToFile writes the provided string into the specified file.
+func writeToFile(target string, input string) error {
+
+	file, err := os.OpenFile(target, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer file.Close()
 
 	if _, err := file.WriteString(input + "\n"); err != nil {
 		log.Fatal(err)
 	}
-
 	if err := file.Close(); err != nil {
 		log.Fatal(err)
 	}
+
 	return nil
 }
 
-func readFromFile(fileName string) error {
+// readFromFile reads the content of a specified file and returns its contents as a string.
+func readFromFile(fileName string) (string, error) {
+
+	log.Printf("Reading from file %v\n", fileName)
 
 	byteArr, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	_ = byteArr
-	// do something with, or return, byteArr
-
-	return nil
+	return string(byteArr), nil
 }
